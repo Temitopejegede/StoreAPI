1c436998df070899bd5f85031c8f809e69a8fa71
temitopejegede <temiteejegede@gmail.com>
2022-07-10 15:59:56 -0400

    added routes and connected to db

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-require("dotenv").config;
+require("dotenv").config();
+require('express-async-errors')
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
 const notFoundMiddleware = require("./middleware/not-found");
-const erroMiddler = require("./middleware/error-handler");
+const errorMiddleware = require("./middleware/error-handler");
+const productRouter = require("./routes/products")
 
 // app.use(express.static("./public"));
 app.use(express.json());
@@ -12,14 +14,16 @@ app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
+app.use('/api/v1/products', productRouter)
+
 app.use(notFoundMiddleware);
-app.use(erroMiddler);
+app.use(errorMiddleware);
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONG0_URI);
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`server is listening on ${port}`));
   } catch (error) {
     console.log(error);
