08296e9b0f88a91846d5444af012279874c928a5
temitopejegede <temiteejegede@gmail.com>
2022-07-17 16:11:33 -0400

    added field parameter

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,15 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort("name");
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(10);
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -23,6 +26,10 @@ const getAllProducts = async (req, res) => {
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
+  }
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+      result = result.select(fieldsList);
   }
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
